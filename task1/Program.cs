70552572855e4c61aed81216e9c82142a6c0d95a
akhmed <akhmed@storyAkhmedMacBook-Air.local>
2022-10-25 15:15:26 +0300

    1

--- task1/Program.cs
+++ task1/Program.cs
@@ -12,11 +12,12 @@ PrintArray(array);
 
 void GetArray(double[,] array)
 {
+    var random = new Random();
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array [i,j] = new Random().Next(-9,9);
+            array [i,j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
         }   
     }
 
